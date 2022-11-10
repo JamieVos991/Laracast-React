7fb203a8865a036b4ba49aee2ae0e3bf68eed2b1
Jamie Vos <Jamievos100@gmail.com>
2022-11-10 17:55:20 +0100

    initial markup and styles for todo app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import './App.css';
-import { useState } from 'react';
-import Another from './Another';
+import Todo from './components/Todo/Todo';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  function decrement() {
-    setCount(prevCount => prevCount - 1);
-  }
-
-  function increment() {
-    setCount(prevCount => prevCount + 1);
-  }
-
-  return (
-    <div className="App">
-      <span>{count}</span>
-      <button onClick={decrement}>-</button>
-      <button onClick={increment}>+</button>
-
-      <Another name="Jamie" />
-      <Another />
-    </div>
-  );
+  return <Todo />;
 }
 
 export default App;
