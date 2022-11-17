69051aa2c75ab98a915b1275961f63736191d574
Jamie Vos <Jamievos100@gmail.com>
2022-11-17 21:02:22 +0100

    When no Todos: show paragraaf with 'Add some todos...'

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -105,95 +105,104 @@ function Todo() {
   }
 
   return (
-    <div>
-      <div className="todo-app-container">
-        <div className="todo-app">
-          <h2>Todo App</h2>
-          <form action="#" onSubmit={addTodo}>
-            <input
-              type="text"
-              value={todoInput}
-              onChange={handleInput}
-              className="todo-input"
-              placeholder="What do you need to do?"
-            />
-          </form>
+    <div className="todo-app-container">
+      <div className="todo-app">
+        <h2>Todo App</h2>
+        <form action="#" onSubmit={addTodo}>
+          <input
+            type="text"
+            value={todoInput}
+            onChange={handleInput}
+            className="todo-input"
+            placeholder="What do you need to do?"
+          />
+        </form>
 
-          <ul className="todo-list">
-            {todos.map((todo, index) => (
-              <li key={todo.id} className="todo-item-container">
-                <div className="todo-item">
-                  <input
-                    type="checkbox"
-                    onChange={() => completeTodo(todo.id)}
-                    checked={todo.isComplete ? true : false}
-                  />
-
-                  {!todo.isEditing ? (
-                    <span
-                      onDoubleClick={() => markAsEditing(todo.id)}
-                      className={`todo-item-label ${
-                        todo.isComplete ? 'line-through' : ''
-                      }`}
-                    >
-                      {todo.title}
-                    </span>
-                  ) : (
+        {todos.length > 0 ? (
+          <>
+            <ul className="todo-list">
+              {todos.map((todo, index) => (
+                <li key={todo.id} className="todo-item-container">
+                  <div className="todo-item">
                     <input
-                      type="text"
-                      onBlur={event => updateTodo(event, todo.id)}
-                      className="todo-item-input"
-                      defaultValue={todo.title}
-                      onKeyDown={event => {
-                        if (event.key === 'Enter') {
-                          updateTodo(event, todo.id);
-                        } else if (event.key === 'Escape') {
-                          cancelEdit(event, todo.id);
-                        }
-                      }}
-                      autoFocus
+                      type="checkbox"
+                      onChange={() => completeTodo(todo.id)}
+                      checked={todo.isComplete ? true : false}
                     />
-                  )}
-                </div>
 
-                <button
-                  onClick={() => deleteTodo(todo.id)}
-                  className="x-button"
-                >
-                  X
-                  <svg
-                    className="x-button-icon"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M6 18L18 6M6 6l12 12"
-                  />
+                    {!todo.isEditing ? (
+                      <span
+                        onDoubleClick={() => markAsEditing(todo.id)}
+                        className={`todo-item-label ${
+                          todo.isComplete ? 'line-through' : ''
+                        }`}
+                      >
+                        {todo.title}
+                      </span>
+                    ) : (
+                      <input
+                        type="text"
+                        onBlur={event => updateTodo(event, todo.id)}
+                        className="todo-item-input"
+                        defaultValue={todo.title}
+                        onKeyDown={event => {
+                          if (event.key === 'Enter') {
+                            updateTodo(event, todo.id);
+                          } else if (event.key === 'Escape') {
+                            cancelEdit(event, todo.id);
+                          }
+                        }}
+                        autoFocus
+                      />
+                    )}
+                  </div>
+
+                  <button
+                    onClick={() => deleteTodo(todo.id)}
+                    className="x-button"
+                  >
+                    X
+                    <svg
+                      className="x-button-icon"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </button>
+                </li>
+              ))}
+            </ul>
+            <div className="check-all-container">
+              <div>
+                <div className="button">Check All</div>
+              </div>
+              <span>3 items remaining</span>
+            </div>
+
+            <div className="other-buttons container">
+              <div>
+                <button className="button filter-button filter-button-active">
+                  All
                 </button>
-              </li>
-            ))}
-          </ul>
-          <div className="check-all-container">
-            <div>
-              <div className="button">Check All</div>
+                <button className="button filter-button">Active</button>
+                <button className="button filter-button">Completed</button>
+              </div>
+              <div>
+                <button className="button">Clear completed</button>
+              </div>
             </div>
-            <span>3 items remaining</span>
+          </>
+        ) : (
+          <div className="no-todos-container">
+            <p>Add some todos...</p>
           </div>
-          <div className="other-buttons container">
-            <div>
-              <button className="button filter-button filter-button-active">
-                All
-              </button>
-              <button className="button filter-button">Active</button>
-              <button className="button filter-button">Completed</button>
-            </div>
-            <button className="button">Clear completed</button>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
