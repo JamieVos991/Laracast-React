85099504bfcc1d296c33f0f2be222bbf72156078
Jamie Vos <Jamievos100@gmail.com>
2022-11-17 16:18:51 +0100

    Code organized in Index.html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import AppClass from './components/Todo/TodoClass/TodoClass';
+// import AppClass from './components/Todo/TodoClass/TodoClass';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppClass />
+    <App />
   </React.StrictMode>
 );
